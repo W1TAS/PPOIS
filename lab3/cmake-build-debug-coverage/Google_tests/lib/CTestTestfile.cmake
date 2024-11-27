5050ee5b9f0633f50181c356575d466d78487e5c
W1TAS <144806982+W1TAS@users.noreply.github.com>
2024-11-27 15:44:20 +0300

    Delete lab3/cmake-build-debug-coverage directory

--- lab3/cmake-build-debug-coverage/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/BSUIR/course2/PPOIS/lab3/Google_tests/lib
-# Build directory: D:/BSUIR/course2/PPOIS/lab3/cmake-build-debug-coverage/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
